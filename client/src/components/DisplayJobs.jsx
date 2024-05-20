735aa8e8ee06ca1c8bb16ac927912ef4df1839ca
Aashish Katila <aashishkatila@gmail.com>
2024-05-20 11:35:46 +0545

    Display all jobs

--- client/src/components/DisplayJobs.jsx
+++ client/src/components/DisplayJobs.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchJobs } from '../redux/jobListSlice'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const DisplayJobs = () => {
 
     const {loading,error,jobList} = useSelector((state) => state.jobList)
     
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     useEffect(()=>{
         dispatch(fetchJobs())
@@ -16,14 +17,15 @@ const DisplayJobs = () => {
     return (
         <>
     <div className='text-green-900 my-8 font-bold text-xl'>Jobs: </div>
-    { loading && !error && <p> Loading</p> }
-    {error && <p>Error: {error}</p>}
-      {jobList.job_list ? (
+    { loading && !error ? <p> Loading</p> : null }
+    {!loading && error && <p>Error: {error}</p>}
+      {!loading && jobList.job_list ? (
         <ul>
          {jobList.job_list.slice(1).map((job, index) => (
             <div className='grid grid-cols-4 my-2'>
             <li key={index}>{job.title}</li>
-            <Link to={`/job/${job.job_id}`}><button className='px-4 py-2 bg-green-400 font-bold w-44' >Apply</button></Link>
+          {console.log(job)}
+            <button className='px-4 py-2 bg-green-400 font-bold w-44' onClick={()=>navigate(`/job/${job.job_id}`)}>Apply</button>
             </div>
          ))}
         </ul>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './authSlice'
 import jobListReducer from './jobListSlice'
+import JobDetailReducer from './jobSlice'
 
 const store = configureStore({
     reducer:{
         auth: authReducer,
-        jobList: jobListReducer
+        jobList: jobListReducer,
+        jobDetail: JobDetailReducer,
     }
 })
 
