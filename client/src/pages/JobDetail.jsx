56269a1662a6cecfc656232c5041545420092211
Aashish Katila <aashishkatila@gmail.com>
2024-05-20 11:36:44 +0545

    Display individual jobs and apply successfull

--- client/src/pages/JobDetail.jsx
+++ client/src/pages/JobDetail.jsx
@@ -1,12 +1,69 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
+import { applyForJob, fetchJobDetails } from '../redux/jobSlice'
 
 const JobDetail = () => {
     const {id} = useParams()
-    const [jobDetails,setJobDetails] = useState(null)
-  return (
-    <div>JobDetail</div>
+
+    const [description,setDescription] = useState('')
+    const [cv,setCV] = useState('')
+
+
+    const {jobDetails,loading,error} = useSelector((state) => state.jobDetail)
+    const {user} = useSelector((state) => state.auth)
+    const userId = user.user.id
+    console.log(userId)
+    const dispatch = useDispatch()
+
+    useEffect(() =>{
+    dispatch(fetchJobDetails(id)) 
+    },[dispatch,id])
+
+    const handleCV = async (e) =>{
+      console.log(e.target.files[0])
+      setCV(e.target.files[0])
+      console.log(cv)
+    }
+
+    const handleApplyJob = (e) =>{
+      e.preventDefault()
+      const formData = new FormData();
+      formData.append('user_id',userId)
+      formData.append('description',description)
+      formData.append('job_id',id)
+      formData.append('pdf',cv)
+      console.log(formData)
+      dispatch(applyForJob(formData)).then((result) =>{
+        if(result.payload){
+          alert("Succesfulyy applied")
+        }else{
+          alert("Failed")
+        }
+      })
+    }
+
+    return (
+    <div className='m-4'>
+      <h2>Job Details </h2>
+       <div>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+            {jobDetails && (
+                <div className=''>
+                    <h1>Title : {jobDetails.title}</h1>
+                    <p>Skill Required : {jobDetails.skills}</p>
+                    <p>Description : {jobDetails.description}</p>
+                    <p>Application ends on : {jobDetails.due_date} xth day mnth,year</p>
+                    <textarea placeholder='Write coverletter' className='mt-2 border-2 w-[400px] focus:outline-none' value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <br />
+                    <input placeholder='Upload your Cv' className='mt-2' type='file' accept='.pdf' onChange={handleCV} />
+                    <br />
+                    <button className='px-4 py-1 bg-green-400 mt-2 ' onClick={handleApplyJob}> Apply</button>
+                </div>
+            )}
+        </div>
+    </div>
   )
 }
 
--- client/src/redux/jobSlice.js
+++ client/src/redux/jobSlice.js
@@ -1,15 +1,31 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-export const fetchJobs = createAsyncThunk('/jobs/fetchJobs',async() =>{
+export const fetchJobDetails = createAsyncThunk('/jobs/fetchJobDetails',async(jobId) =>{
+    
     try{
         const token = localStorage.getItem("token")
-        console.log("Hello " ,token )
-        const res = await axios.get("http://127.0.0.1:8000/api/user/job_list",{
+        const res = await axios.get(`http://127.0.0.1:8000/api/job/${jobId}`,{
             headers:{
                 Authorization: `Bearer ${token}`
             },
         })
+        console.log(res.data.job)
+        return res.data.job
+    }catch(error){
+        throw(error)
+    }
+})
+
+export const applyForJob = createAsyncThunk('./jobs/applyForJob',async(applicantData) =>{
+    try{
+        const token = localStorage.getItem("token")
+        const res = await axios.post('http://127.0.0.1:8000/api/user/form',applicantData,{
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        })
+        console.log(res.data)
         return res.data
     }catch(error){
         throw(error)
@@ -17,26 +33,40 @@ export const fetchJobs = createAsyncThunk('/jobs/fetchJobs',async() =>{
 })
 
 const jobSlice = createSlice({
-    name: 'jobs',
-    initialState:{
-        loading: null,
-        jobList: [],
-        error: null,
+    name : 'job',
+    initialState : {
+        loading : null,
+        jobDetails : null, 
+        error : null,
+        applyLoading : null,
+        applyError : null,
+        applySuccess : null,
     },
     extraReducers: (builder) =>{
         builder
-        .addCase(fetchJobs.pending,(state) =>{
+        .addCase(fetchJobDetails.pending,(state) =>{
             state.loading = true;
             state.error = false;
         })
-        .addCase(fetchJobs.fulfilled,(state,action) =>{
+        .addCase(fetchJobDetails.fulfilled,(state,action) =>{
             state.loading = false;
-            state.jobList = action.payload;
+            state.jobDetails = action.payload;
             state.error = false
         })
-        .addCase(fetchJobs.rejected,(state,action) =>{
+        .addCase(fetchJobDetails.rejected,(state,action) =>{
             state.pending = false;
             state.error = action.error.message
+        })
+        .addCase(applyForJob.pending,(state) =>{
+            state.applyLoading = true
+        })
+        .addCase(applyForJob.fulfilled,(state,action) =>{
+            state.applyLoading = false;
+            state.applySuccess = action.payload;
+        })
+        .addCase(applyForJob.rejected,(state) =>{
+            state.applyLoading = false;
+            state.applyError = true;
         })
     }
 })
