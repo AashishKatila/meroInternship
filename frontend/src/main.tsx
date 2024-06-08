ada9b36f3a16b8b52e2616811543ad55159d74d7
Aashish Katila <aashishkatila@gmail.com>
2024-06-08 19:13:14 +0545

    Remove tanstack

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,18 +5,13 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
