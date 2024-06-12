876b36e8fcd4553b4395c692973bbf985fff7167
Aashish Katila <aashishkatila@gmail.com>
2024-06-12 21:39:52 +0545

    Configure routes fro company authentication

--- frontend/src/Routes/AfterAuth.tsx
+++ frontend/src/Routes/AfterAuth.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "@/Pages/Dashboard";
+import CompanyDashboard from "@/Pages/company/CompanyDashboard";
 
 const AfterAuth = () => {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/company/dashboard" element={<CompanyDashboard />} />
       <Route path="*" element={<Dashboard />} />
     </Routes>
   );
--- frontend/src/Routes/BeforeAuth.tsx
+++ frontend/src/Routes/BeforeAuth.tsx
@@ -3,6 +3,8 @@ import Home from "@/Pages/Home";
 import Login from "@/Pages/Login";
 import Signup from "@/Pages/Signup";
 import Jobs from "@/Pages/Jobs";
+import CompanyLogin from "@/Pages/company/CompanyLogin";
+import CompanySignup from "@/Pages/company/CompanySignup";
 
 const BeforeAuth = () => {
   return (
@@ -11,6 +13,8 @@ const BeforeAuth = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/jobs" element={<Jobs />} />
+      <Route path="/company/login" element={<CompanyLogin />} />
+      <Route path="/company/signup" element={<CompanySignup />} />
       <Route path="*" element={<Login />} />
     </Routes>
   );
